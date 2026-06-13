888d0e01444f20a822444549346bb96d628aa417
adamtfa <adam.tuffaha@outlook.de>
2026-06-13 20:03:20 +0200

    M4

--- src/components/TableList.vue
+++ src/components/TableList.vue
@@ -8,6 +8,12 @@
       </li>
     </ul>
   </div>
+  <div>
+    <input v-model="tableNumberField" type="number" placeholder="Table Number" />
+    <input v-model="capacityField" type="number" placeholder="Capacity" />
+    <input v-model="isAvailableField" type="checkbox" />
+    <button @click="save">Add Table</button>
+  </div>
 </template>
 
 <script setup lang="ts">
@@ -25,7 +31,7 @@ defineProps<{
 
 const tables = ref<RestaurantTable[]>([])
 
-onMounted(() => {
+function loadTables() {
   const endpoint = import.meta.env.VITE_APP_BACKEND_BASE_URL + '/tables'
   const requestOptions = {
     method: 'GET',
@@ -37,7 +43,36 @@ onMounted(() => {
       tables.value = result
     })
     .catch((error) => console.log('error', error))
-})
+}
+
+onMounted(() => loadTables())
+
+const tableNumberField = ref<number>(0)
+const capacityField = ref<number>(0)
+const isAvailableField = ref<boolean>(false)
+
+function save() {
+  const endpoint = import.meta.env.VITE_APP_BACKEND_BASE_URL + '/tables'
+  const data = {
+    tableNumber: tableNumberField.value,
+    capacity: capacityField.value,
+    isAvailable: isAvailableField.value,
+  }
+  const requestOptions = {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  }
+  fetch(endpoint, requestOptions)
+    .then((response) => response.json())
+    .then((data) => {
+      console.log('Success:', data)
+      loadTables()
+    })
+    .catch((error) => console.log('error', error))
+}
 </script>
 
 <style scoped>
